3837065c05769d5340ac7bd1d5d7d5717ab3c95f
hunterdevelopeer <164854491+hunterdevelopeer@users.noreply.github.com>
2024-03-25 05:15:50 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
-/* global dataLayer */
-import React, { useEffect } from 'react';
 import './App.css';
 import Header from './Header';
-import Home from './Home';
+import Home from './Home'
 
 function App() {
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-LFCKZTFPJT';
-    script.async = true;
-    document.head.appendChild(script);
-
-    dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'G-LFCKZTFPJT');
-
-    return () => {
-      // Limpar script se o componente for desmontado
-      document.head.removeChild(script);
-    };
-  }, []);
-
   return (
     <div className="App">
       <Header/>
-      <Home />
+        <Home />
     </div>
   );
 }
